0f6fa6436f95187f0737c32471782a1325d26485
Dshivamthakur <88136860+Dshivamthakur@users.noreply.github.com>
2023-05-13 14:23:45 +0530

    Updates

---
+++ precache-manifest.a7c2979506e43ca087e5424b024917d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e847be2794ad564bb4",
+    "url": "/Textutils/static/css/main.bbf6cc7b.chunk.css"
+  },
+  {
+    "revision": "32e847be2794ad564bb4",
+    "url": "/Textutils/static/js/main.56692dcf.chunk.js"
+  },
+  {
+    "revision": "b5305c03e5a04519dd92",
+    "url": "/Textutils/static/js/runtime~main.a6dbe6f9.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/Textutils/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/Textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "bc7a88e2bc82272028ecfdf17c493d8b",
+    "url": "/Textutils/index.html"
+  }
+];
